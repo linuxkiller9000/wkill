e30661e13e2972b325318e19dafe198b21805db0
honey_block <awadn5287@gmail.com>
2026-06-02 18:34:49 +0300

    Add files via upload

--- wkill.sh
+++ wkill.sh
@@ -1,65 +1,20 @@
-
+#!/bin/bash
+echo "You have only 2 seconds to focus the window you want to close"
 desktop=$(echo "$XDG_CURRENT_DESKTOP" | tr '[:upper:]' '[:lower:]')
-
 NIRI(){
-if [[ $desktop = "niri" ]]; then
-    echo "you got only 4 seconds to focus the window"
-    sleep 4 && niri msg action close-window
-fi
-}
-SWAY(){
-if [[ $desktop = "sway" || $desktop = "volare" || $desktop = "scroll" || $desktop = "swayfx" ]]; then
-    echo "you got 4 seconds to focus the window"
-    sleep 4 && swaymsg kill
-fi
-}
-NILE(){
-if [[ $desktop = "river" || $desktop = "croffle" ]]; then
-    echo "you got 4 seconds to focus the window"
-    sleep 4 && riverctl close
-fi
-}
-QTILE(){
-if [[ $desktop = "qtile" ]]; then
-    echo "you got 4 seconds to focus the window"
-    sleep 4 && qtile cmd-obj -o window -f kill
-fi
-}
-KDE(){
-if [[ $desktop = "kde" || $desktop = "plasma" || $desktop = "theseus" ]]; then
-    echo "you got 4 seconds to focus the window"
-    sleep 4 && qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut "Window Close"
-fi
-}
-GNOME(){
-if [[ $desktop = "gnome" || $desktop = "mutter" || $desktop = "muffin" || $desktop = "magpie" || $desktop = "ukwm" ]]; then
-    echo "you got 4 seconds to focus the window"
-    sleep 4 && wmctrl -c :ACTIVE:
-fi
-}
-HYPR(){
-if [[ $desktop = "hyprland" ]]; then
-    echo "you got 4 seconds to focus the window"
-    sleep 4 && hyprctl dispatch closewindow active
+PID=$(sleep 2 && niri msg --json focused-window | jq '.pid')
+if [[ $PID = "null" ]]; then
+    echo "Nothing to do"
+else
+    echo "Target found with PID of $PID"
+    kill -9 "$PID"
+    echo "Target eliminated"
 fi
 }
 
+
+
 if [[ $desktop = "niri" ]]; then
+    echo "desktop found"
     NIRI
-elif [[ $desktop = "sway" || $desktop = "volare" || $desktop = "scroll" || $desktop = "swayfx" ]]; then
-    SWAY
-elif [[ $desktop = "river" || $desktop = "croffle" ]]; then
-    NILE
-elif [[ $desktop = "qtile" ]]; then
-    QTILE
-elif [[ $desktop = "hyprland" ]]; then
-    HYPR
-elif [[ $desktop = "gnome" || $desktop = "mutter" || $desktop = "muffin" || $desktop = "magpie" || $desktop = "ukwm" ]]; then
-    GNOME
-elif [[ $desktop = "kde" || $desktop = "plasma" || $desktop = "theseus" ]]; then
-    KDE
 fi
-
-
-
-
